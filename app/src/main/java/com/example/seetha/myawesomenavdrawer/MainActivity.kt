cf3a764f65ad67d05390ed98585ba3f132c03761
seethaa <seetha.annamraju@gmail.com>
2018-08-21 22:02:50 -0700

    part 3 ch4: Improve your nav drawer

--- app/src/main/java/com/example/seetha/myawesomenavdrawer/MainActivity.kt
+++ app/src/main/java/com/example/seetha/myawesomenavdrawer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.seetha.myawesomenavdrawer
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         configureToolBar()
         configureDrawerLayout()
         configureNavigationView()
+
+        showFirstFragment()
     }
 
     override fun onBackPressed() {
@@ -31,16 +34,33 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
-        when (item.itemId) {
-            R.id.activity_main_drawer_news -> null
-            R.id.activity_main_drawer_profile -> null
-            R.id.activity_main_drawer_settings -> null
+        val fragment = when (item.itemId) {
+            R.id.activity_main_drawer_news -> NewsFeedFragment.newInstance()
+            R.id.activity_main_drawer_profile -> ProfilePageFragment.newInstance()
+            R.id.activity_main_drawer_settings -> SettingsFragment.newInstance()
             else -> null
         }
 
-        activity_main_drawer_layout.closeDrawer(GravityCompat.START)
+        if (fragment != null) showFragment(fragment)
 
+        activity_main_drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun showFirstFragment() {
+        val visibleFragment = supportFragmentManager.findFragmentById(R.id.activity_main_frame_layout)
+        if (visibleFragment == null) {
+            showFragment(NewsFeedFragment.newInstance())
+            activity_main_nav_view.setCheckedItem(0)
+        }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        if (!fragment.isVisible) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.activity_main_frame_layout, fragment).commit()
+        }
+
     }
 
     private fun configureToolBar() {
@@ -50,13 +70,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private fun configureDrawerLayout() {
         val toggle = ActionBarDrawerToggle(this, activity_main_drawer_layout,
                 activity_main_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        activity_main_drawer_layout.addDrawerListener(toggle);
+        activity_main_drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
     }
 
     private fun configureNavigationView() {
-        activity_main_nav_view.setNavigationItemSelectedListener(this);
-
+        activity_main_nav_view.setNavigationItemSelectedListener(this)
     }
 
 }
---
+++ app/src/main/java/com/example/seetha/myawesomenavdrawer/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.seetha.myawesomenavdrawer
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SettingsFragment : Fragment() {
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = SettingsFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+}
